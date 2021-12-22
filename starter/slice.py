52cafef5ee25a6f955d7016e52e14a27a4506ea0
ed04alb@gmail.com <ed04alb@gmail.com>
2021-12-21 20:11:12 -0600

    fix slice

--- starter/slice.py
+++ starter/slice.py
@@ -4,21 +4,22 @@ import json
 
 def performance_feature(test,encoder,lb, model, cat_features):
 
-    performance = {"feature":[],"precision":[],"recall":[],"fbeta":[]}
+    results = {}
     for feature in cat_features:
-        X = test[feature]
-        X, y_test, encoder, lb = process_data(
-        test, categorical_features=cat_features, label="salary", training=False,
-        encoder = encoder, lb = lb)
-        preds = inference(model, X)
-        precision, recall, fbeta = compute_model_metrics(y_test, preds)
-        performance['feature'].append(feature)
-        performance['precision'].append(precision)
-        performance['recall'].append(recall)
-        performance['fbeta'].append(fbeta)
+        performance = {}
+        for label in test[feature].unique():
+            data = test[test[feature] == label]
+            X, y_test, encoder, lb = process_data(
+            data, categorical_features=cat_features, label="salary", training=False,
+            encoder = encoder, lb = lb)
+            preds = inference(model, X)
+            precision, recall, fbeta = compute_model_metrics(y_test, preds)
+            performance[label] = {'precision':precision,'recall':recall,'fbeta':fbeta}
+        results[feature] = performance
+        
     
-    with open('../screenshots/slice_outouts.txt', 'w') as file:
-        file.write(json.dumps(performance)) # use `json.loads` to do the reverse
+    with open('../screenshots/slice_output.txt', 'w') as file:
+        file.write(json.dumps(results)) # use `json.loads` to do the reverse
 
 
 
